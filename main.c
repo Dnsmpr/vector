9875cba26a9a44834d2703a032cb34c357835d04
Dnsmpr <104632579+Dnsmpr@users.noreply.github.com>
2024-02-02 03:18:04 +0100

    Add new function calls for testing.

--- main.c
+++ main.c
@@ -9,9 +9,11 @@ typedef struct test {
 
 int main(int argc, char* argv[]) {
     uint64_t cap = 4;
-    size_t s = sizeof(int);
-    vector* v = vec_init(cap, s);
     char* v1 = "aa";
+    size_t s = sizeof(strlen(v1) + 1);
+    vector* v = vec_init(cap, s);
+    char* n10 = "xx";
+    char* n1 = "tt";
     char* v2 = "bb";
     char* v3 = "cc";
     char* v4 = "dd";
@@ -21,6 +23,7 @@ int main(int argc, char* argv[]) {
     v->append(v1, v);
     v->append(v2, v);
     v->append(v3, v);
+
     v->append(v4, v);
     v->append(v5, v);
     v->append(v6, v);
@@ -28,13 +31,29 @@ int main(int argc, char* argv[]) {
     char* v8 = "zz";
     v->front(v8, v);
     char* v9 = "hh";
+    add_at_index(v, 5, v9);
+    add_at_index(v, 1, v9);
+    add_at_index(v, 2, v9);
     add_at_index(v, 3, v9);
+    v->append(v1, v);
+    v->append(v1, v);
+    v->front(v1, v);
+    v->front(v1, v);
+
+    v->append(n1, v);
+    v->front(n1, v);
+    replace(v, 1, n10);
+    replace(v, 100, n10);
+    replace(v, 5, n10);
+
+
     v->print(v);
     printf("%ld\n", v->get_size(v));
     printf("%d\n", v->is_empty(v));
     char* val = v->pop(v);
     printf("%s\n", val);
     printf("%ld\n", v->get_size(v));
+    free(val);
 
     v->destroy(v);
     return 0;
